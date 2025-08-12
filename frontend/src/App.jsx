2f815fa72001e799d569d7ae7903ab2f8a6c42bb
Harshavardhanreddy <harshavardhanreddyvanukuri26@gmail.com>
2025-08-12 23:08:07 +0530

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import HomeLayout from './components/Layout/HomeLayout'
+
 
 function App() {
   
 
   return (
     <>
-     
+     <Routes>
+      <Route path="/" element={<HomeLayout />} />
+      <Route>
+        {/* Admin Layout */}
+
+      </Route>
+     </Routes>
     </>
   )
 }
