6d35362b198a00e4543ff98430644be62374e6b0
Harshavardhanreddy <harshavardhanreddyvanukuri26@gmail.com>
2025-08-13 23:34:19 +0530

    fifth

--- frontend/src/components/CommonPages/Header.jsx
+++ frontend/src/components/CommonPages/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 function Header() {
   return (
-    <>
+    <header>
       <Topbar />
-    </>
+      <Navbar />
+    </header>
   )
 }
 
---
+++ frontend/src/components/CommonPages/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {HiOutlineUser,HiOutlineShoppingCart,HiBars3BottomRight} from 'react-icons/hi2'
+import Searchbar from './Searchbar'
+
+function Navbar() {
+  return (
+    <>
+    <nav className='container mx-auto flex items-center justify-between py-4 px-6'>
+     <div>
+          <Link to="/" className='text-2xl font-medium'>Furnish Store</Link>
+     </div>
+     <div className='hidden md:flex space-x-6'>
+          <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Living room </Link>
+          <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Bedroom </Link>
+          <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Dining room </Link>
+          <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Office room </Link>
+
+     </div>
+     <div className='flex items-center space-x-4'>
+          <Link to="/profile" className='hover:text-black'>
+          <HiOutlineUser  className='h-6 w-6  text-gray-700'/>
+          </Link>
+          <button className='relative hover:text-black'>
+               <HiOutlineShoppingCart  className='h-6 w-6 text-gray-700'/>
+               <span className='absolute -top-1 bg-[#ea2e0e] text-white text-xs rounded-full px-2 py-0.5 cursor-pointer'>
+                    4
+
+               </span>
+
+          </button>
+          <div className="overflow-hidden">
+               <Searchbar/>
+          </div>
+          
+          <button className='md:hidden'>
+               <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
+          </button>
+
+
+     </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
